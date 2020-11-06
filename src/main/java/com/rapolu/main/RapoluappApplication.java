0d39a78df8a9e0f17c293136efe930d3ea114ed7
saipr <saipr@DESKTOP-BD7FNVN>
2020-11-06 15:13:03 +0530

    rapolu application creation

---
+++ src/main/java/com/rapolu/main/RapoluappApplication.java
@@ -0,0 +1,13 @@
+package com.rapolu.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RapoluappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RapoluappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rapolu/main/RapoluappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rapolu.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RapoluappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
